5e27b292cd6f1d4a6e497d9cc6be5b9d6e02900b
Richard Roberson <me@richardr.dev>
2025-03-13 00:02:01 -0600

    Each consumer gets a load balancer and dns name

--- web/server.py
+++ web/server.py
@@ -29,11 +29,11 @@ try:
         bootstrap_servers=os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'localhost:9092'),
         value_serializer=lambda v: json.dumps(v).encode('utf-8'),
         client_id=client_id,
-        #security_protocol='SSL',
-        #ssl_check_hostname=False,
-        #ssl_certfile='certs/tls.crt',
-        #ssl_keyfile='certs/tls.key',
-        #ssl_cafile='certs/ca.crt',  # Add this line
+        security_protocol='SSL',
+        ssl_check_hostname=False,
+        ssl_certfile='certs/tls.crt',
+        ssl_keyfile='certs/tls.key',
+        ssl_cafile='certs/ca.crt',  # Add this line
         #ssl_password='kafka123',
         max_in_flight_requests_per_connection=1,  # Ensure ordering
     )
